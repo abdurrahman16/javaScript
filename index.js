4a7ceb3d18f7e6af3a294854d803033227d2b88d
Mohammad Abdur Rahman <rahman.just16@gmail.com>
2021-01-16 00:49:13 +0600

    jS basic

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
-// var banaNa=1211;
-// console.log(banaNa);
-// var yourName=true;
-// console.log(typeof yourName);
-// console.log("hello");
-// //practising
+var banaNa=1211;
+console.log(banaNa);
+var yourName=true;
+console.log(typeof yourName);
+console.log("hello");
+//practising
 
-// var promise="i will work  hard";
-// console.log(promise.toLocaleUpperCase);
-// console.log("hello");
-// var babu="baBu";
-// console.log(babu.toUpperCase());
+var promise="i will work  hard";
+console.log(promise.toLocaleUpperCase);
+console.log("hello");
+var babu="baBu";
+console.log(babu.toUpperCase());
 
-// var a=0.3;
-// var b=0.3
-// sum= a+b;
-// sum=sum.toFixed(4);
-// console.log(sum);
-
-
-// var price1 = 10;
-// var price2 = 3;
-// price2= price2+1;
-
-// var sum= price1 + price2;
-// var sub= price1 - price2;
-// var mul= price1 * price2;
-// var div= price1 / price2;
-// var rem= price1 % price2;
+var a=0.3;
+var b=0.3
+sum= a+b;
+sum=sum.toFixed(4);
+console.log(sum);
 
 
-// console.log(sum);
-// console.log(sub);
-// console.log(mul);
-// console.log(div.toFixed(2));
-// console.log(rem.toFixed(2));
+var price1 = 10;
+var price2 = 3;
+price2= price2+1;
 
-// console.log(sum);
-// console.log(sub);
-// console.log(mul);
-// console.log(div.toFixed(2));
-// console.log(rem.toFixed(2));
+var sum= price1 + price2;
+var sub= price1 - price2;
+var mul= price1 * price2;
+var div= price1 / price2;
+var rem= price1 % price2;
+
+
+console.log(sum);
+console.log(sub);
+console.log(mul);
+console.log(div.toFixed(2));
+console.log(rem.toFixed(2));
+
+console.log(sum);
+console.log(sub);
+console.log(mul);
+console.log(div.toFixed(2));
+console.log(rem.toFixed(2));
 
 var number= -5.49;
 var number2= 100;
@@ -62,22 +62,46 @@ var absoluteNumber2 = Math.random(number2);
 
  var vx= console.log(absoluteNumber2);
 
-// absoluteNumber2= Math.floor(number2);
+absoluteNumber2= Math.floor(number2);
 
 console.log(vx);
 
+var b=12;
+
+if(b==112){
+    console.log("i will go")
+}
+else {
+    console.log("na jamuna");
+}
+
+b++;
+if(b !=03)
+{
+    console.log("hehe");
+}
+else{
+    console.log("opps");
+}
 
 
 
+var job= true;
+var salary= 5000;
+
+if( job== true && salary>20000){
+     console.log("biya hobe");
+}
+else if(job==false){
+    console.log("will try")
+}
+else {
+    console.log("biya hobe na re bhai");
+}
 
 
-
-
-
-
-
-
-
+var date= new Date ();
+console.log(date);
 
 
 
