f2732c172a7798bfd080bbec627663e9bd7ab863
Mohammad Abdur Rahman <rahman.just16@gmail.com>
2021-01-16 00:18:25 +0600

    js

---
+++ index.js
@@ -0,0 +1,76 @@
+// var banaNa=1211;
+// console.log(banaNa);
+// var yourName=true;
+// console.log(typeof yourName);
+// console.log("hello");
+// //practising
+
+// var promise="i will work  hard";
+// console.log(promise.toLocaleUpperCase);
+// console.log("hello");
+// var babu="baBu";
+// console.log(babu.toUpperCase());
+
+// var a=0.3;
+// var b=0.3
+// sum= a+b;
+// sum=sum.toFixed(4);
+// console.log(sum);
+
+
+// var price1 = 10;
+// var price2 = 3;
+// price2= price2+1;
+
+// var sum= price1 + price2;
+// var sub= price1 - price2;
+// var mul= price1 * price2;
+// var div= price1 / price2;
+// var rem= price1 % price2;
+
+
+// console.log(sum);
+// console.log(sub);
+// console.log(mul);
+// console.log(div.toFixed(2));
+// console.log(rem.toFixed(2));
+
+// console.log(sum);
+// console.log(sub);
+// console.log(mul);
+// console.log(div.toFixed(2));
+// console.log(rem.toFixed(2));
+
+var number= -5.09;
+var absoluteNumber = Math.abs(number);
+console.log(absoluteNumber);
+
+var absoluteNumber = Math.round(number);
+console.log(absoluteNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
